c32ec4ecc628260f8aef0f86d9bbe42a1339c6bd
SavaIv <tie.mail@abv.bg>
2022-04-17 23:14:24 +0300

    Add project files.

---
+++ LawOffice05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LawOffice05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
